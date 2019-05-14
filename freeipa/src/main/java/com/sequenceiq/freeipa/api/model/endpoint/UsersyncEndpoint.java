9530e06ed7eb4e3db1e45eb8358e6fd0ef3f1174
Abhishek Purohit <apurohit@cloudera.com>
2019-05-14 14:23:26 -0700

    Refactored and added UserdateUser Req Res

--- freeipa/src/main/java/com/sequenceiq/freeipa/api/model/endpoint/UsersyncEndpoint.java
+++ freeipa/src/main/java/com/sequenceiq/freeipa/api/model/endpoint/UsersyncEndpoint.java
@@ -1,4 +1,4 @@
-package com.sequenceiq.freeipa.api.model.users;
+package com.sequenceiq.freeipa.api.model.endpoint;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -8,6 +8,11 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.sequenceiq.freeipa.api.model.ContentType;
+import com.sequenceiq.freeipa.api.model.users.SynchronizeUsersRequest;
+import com.sequenceiq.freeipa.api.model.users.SynchronizeUsersResponse;
+import com.sequenceiq.freeipa.api.model.users.SynchronizeUsersStatus;
+import com.sequenceiq.freeipa.api.model.users.UpdateUsersRequest;
+import com.sequenceiq.freeipa.api.model.users.UpdateUsersResponse;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -28,4 +33,10 @@ public interface UsersyncEndpoint {
     @Produces(MediaType.APPLICATION_JSON)
     @ApiOperation(value = "Get status of latest synchronization", produces = ContentType.JSON, nickname = "getStatus")
     SynchronizeUsersStatus getStatus();
+
+    @POST
+    @Path("/user/{user_name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Updates user's data into FreeIPA, user must be existing", produces = ContentType.JSON, nickname = "updateUser")
+    UpdateUsersResponse updateUser(UpdateUsersRequest request);
 }
